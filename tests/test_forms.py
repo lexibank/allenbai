d8ed550f59b15df82d1a9a67c66a22355d8a6967
xrotwang <xrotwang@googlemail.com>
2018-04-18 12:53:22 +0200

    fixed inconsistency

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -5,5 +5,5 @@ from __future__ import unicode_literals, print_function
 def test_contains(cldf_dataset, log):
     assert cldf_dataset.validate(log=log)
     assert any(f['Form'] == 'mi³⁵zɔ²¹' for f in cldf_dataset['FormTable'])
-    assert len(list(cldf_dataset['FormTable'])) >= 3000
+    assert len(list(cldf_dataset['FormTable'])) == 4546
 
