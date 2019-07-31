48ce25c6a119fec7fcbca3533723dbaf3310778f
LinguList <mattis.list@posteo.de>
2019-07-31 19:45:56 +0200

    upate language names

--- lexibank_allenbai.py
+++ lexibank_allenbai.py
@@ -3,7 +3,7 @@ import lingpy
 from clldutils.misc import lazyproperty
 from clldutils.path import Path
 from lingpy.sequence.sound_classes import clean_string, syllabify
-from pylexibank.dataset import Concept
+from pylexibank.dataset import Concept, Language
 from pylexibank.dataset import Dataset as BaseDataset
 from pylexibank.util import pb, getEvoBibAsBibtex
 
@@ -12,11 +12,19 @@ from pylexibank.util import pb, getEvoBibAsBibtex
 class BaidialConcept(Concept):
     Chinese = attr.ib(default=None)
 
+@attr.s
+class HLanguage(Language):
+    Latitude = attr.ib(default=None)
+    Longitude = attr.ib(default=None)
+    ChineseName = attr.ib(default=None)
+    SubGroup = attr.ib(default=None)
+    Family = attr.ib(default=None)
 
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
     id = "allenbai"
     concept_class = BaidialConcept
+    language_class = HLanguage
 
     def clean_form(self, row, form):
         form = self.lexemes.get(form.strip(), form.strip())
@@ -34,11 +42,27 @@ class Dataset(BaseDataset):
             ds.add_concepts(id_factory=lambda c: c.number)
             concept2id = {c.english: c.number for c in self.conceptlist.concepts.values()}
             ds.add_languages(id_factory=lambda l: l["Name"])
+
+            langs = {}
+            for language in self.languages:
+                ds.add_language(
+                        ID=language['ID'],
+                        Glottocode=language["Glottocode"],
+                        Name=language["Name"],
+                        Latitude=language['Latitude'],
+                        Longitude=language['Longitude'],
+                        SubGroup=language['SubGroup'],
+                        ChineseName=language['ChineseName'],
+                        Family='Sino-Tibetan'
+                    )
+                langs[language["Name"]] = language['ID']
+
+
             ds.add_sources()
             for k in pb(wl, desc="wl-to-cldf"):
                 if wl[k, "value"]:
                     ds.add_lexemes(
-                        Language_ID=wl[k, "doculect"],
+                        Language_ID=langs[wl[k, "doculect"]],
                         Parameter_ID=concept2id[wl[k, "concept"]],
                         Value=wl[k, "value"],
                         Source="Allen2007",
