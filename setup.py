1b5699fff1f63eee50f05a5886b03e77c816af24
chrzyki <christoph@functor-argument.net>
2019-11-11 09:28:03 +0100

    Release prep, updated tests and setup, re-generated CLDF

--- setup.py
+++ setup.py
@@ -2,30 +2,19 @@ from setuptools import setup
 import json
 
 
-with open('metadata.json') as fp:
+with open("metadata.json") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_allenbai',
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_allenbai'],
+    name="lexibank_allenbai",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_allenbai"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'allenbai=lexibank_allenbai:Dataset',
-        ]
-    },
-    install_requires=[
-        'pylexibank>=1.1.1',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf'
-        ],
-    }
+    entry_points={"lexibank.dataset": ["allenbai=lexibank_allenbai:Dataset"]},
+    install_requires=["pylexibank>=2.1"],
+    extras_require={"test": ["pytest-cldf"]},
 )
-
